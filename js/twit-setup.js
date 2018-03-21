4a4d9bc58fdb38f30a5bcaf43fc8ea5e6f634a6c
jonathanimo <jonathanimo89@gmail.com>
2018-03-21 09:25:50 -0400

    lol
    whoops

--- js/twit-setup.js
+++ js/twit-setup.js
@@ -2,9 +2,9 @@ var Twit = require('twit')
  
 var T = new Twit({
     consumer_key:         '8FS7scToNuDZQ83mSkmiFEAdz'
-  , consumer_secret:      'ilEBGWZ2VMGGyvAfSAVWbd87ebqpDj9RxDXQddND15SJ6Cgsq0'
+  , consumer_secret:      'CONSUMERSECRET'
   , access_token:         '180362622-UZb5Xsc534TVAg6d6WHCOyN7PjDuY7U608WrySOB'
-  , access_token_secret:  'SXA1HXupJLY17tiwPv1BwfHbhQoF8sf6mBJ4gPnl2oMul'
+  , access_token_secret:  'SECRETTOKEN'
 })
  
 // 
